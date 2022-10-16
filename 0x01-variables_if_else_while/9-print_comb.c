1aaec415dfbad308a8b371d5c8aea31c3ef47620
kingswaysales <marco.nkululeko@gmail.com>
2022-10-16 02:00:41 -0700

    9-print_comb.c

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -9,16 +9,16 @@ int main(void)
 
 	int n;
 
-	for (n = 0; n < 9; n++)
+	for (n = 0, n < 9, n++)
 	{
 		putchar(n);
-		if (n != 9)
+		if (n != 7)
 		{
 
 			putchar(',');
 			putchar(' ');
 		}
-
+	}
 	putchar('\n');
 
 	return (0);
