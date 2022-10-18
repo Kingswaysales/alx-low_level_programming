8712dfbf1647492bbf42c7cfa121f87cc725f5e3
kingswaysales <marco.nkululeko@gmail.com>
2022-10-17 19:39:46 -0700

    9-print_comb.c

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -3,14 +3,14 @@
 
 /**
  *  main - prints a series of digits with commais
- *  Return: Always 0
+ *  Return: Always (Success);
  */
 int main(void)
 {
 
 	int n;
 
-	for (n = 48, n < 58, n++)
+	for (n = 48; n < 58; n++)
 	{
 		putchar(n);
 		if (n != 57)
