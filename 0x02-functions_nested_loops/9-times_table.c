782ca14f2a76fd9687ce0167686af9fe3033c9e8
kingswaysales <marco.nkululeko@gmail.com>
2022-10-18 19:35:28 -0700

    9-times_table.c

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -7,7 +7,7 @@ void times_table(void)
 {
 	int num, mult, prod;
 
-	for (num = 0; num <= 0; num++)
+	for (num = 0; num <= 9; num++)
 	{
 		_putchar('0');
 
@@ -23,6 +23,7 @@ void times_table(void)
 
 			else
 				_putchar((prod / 10) + '0');
+
 			_putchar((prod % 10) + '0');
 		}
 		_putchar('\n');
