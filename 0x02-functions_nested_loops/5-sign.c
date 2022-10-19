a766f4d8b72067455f727b45b5e08a2fd00ea3b8
kingswaysales <marco.nkululeko@gmail.com>
2022-10-18 18:25:36 -0700

    5-sign.c

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -9,19 +9,23 @@
  */
 int print_sign(int n)
 {
+
 	if (n > 0)
 	{
 		_putchar('+');
 		return (1);
+
 	}
 	else if (n == 0)
 	{
 		_putchar('0');
 		return (0);
+
 	}
 	else
 	{
-		_putchar('_');
+		_putchar('-');
 		return (-1);
+
 	}
 }
