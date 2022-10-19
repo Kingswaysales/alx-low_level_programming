d02eda6775a816561f528359c2ace0ac4b7a953c
kingswaysales <marco.nkululeko@gmail.com>
2022-10-18 20:05:36 -0700

    7-print_last_digit.c

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -7,12 +7,12 @@
  */
 int print_last_digit(int n)
 {
-	intlast_digit = n % 10;
+	int ld = n % 10;
 
-	if (last_digit < 0)
-		last_digit *= -1;
+	if (ld < 0)
+		ld *= -1;
 
-	_putchar(last_digit + '0');
+	_putchar(ld + '0');
 
-	return (last_digit);
+	return (0);
 }
