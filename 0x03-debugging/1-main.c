d4a0d8ba6ccdca9e2c3486f4da9a33ca4fe57fe6
kingswaysales <marco.nkululeko@gmail.com>
2022-10-20 13:59:02 -0700

    1-main.c

--- 0x03-debugging/1-main.c
+++ 0x03-debugging/1-main.c
@@ -7,12 +7,12 @@
 
 int main(void)
 {
+
 	int i;
 
 	printf("Infinite loop incoming :(\n");
 
 	i = 0;
-
 	/**
 	*while (i < 10)
 	*{
@@ -22,4 +22,5 @@ int main(void)
 	printf("Infinite loop avoided: \\o/\n");
 
 	return (0);
+
 }
