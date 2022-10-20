41bde555a29827d607f171975cc67f4969d06d22
kingswaysales <marco.nkululeko@gmail.com>
2022-10-20 16:31:41 -0700

    0-isupper.c

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
-* _isupper - checks if a character is uppercase
+* _isupper - check if a character is uppercase
 * @c: Variable text
 * Return: 1 for uppercase or 0 for anthing else
 */
@@ -10,9 +10,8 @@ int _isupper(int c)
 	if (c >= 'A' && c <= 'Z')
 	{
 		return (1);
-	}
-	else
-	{
+
+		}
+
 		return (0);
-	}
 }
