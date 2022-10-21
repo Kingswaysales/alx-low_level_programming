e085d68b9fa3d855ce86192491153fced6b4af6b
kingswaysales <marco.nkululeko@gmail.com>
2022-10-20 18:44:04 -0700

    0-isupper.c

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -5,14 +5,15 @@
  * @c: Variable text
  * Return: 1 if c is uppercase or 0 otherwise
  */
-int _isupper(int c);
+
+int _isupper(int c)
 {
 	if (c >= 'A' && c <= 'Z')
 	{
-		return (1);
+	return (1);
 	}
 	else
 	{
-		return (0);
+	return (0);
 	}
 }
