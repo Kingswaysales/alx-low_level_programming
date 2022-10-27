7d14607c071f276dcff2721a9a1dc4790f37fd91
kingswaysales <marco.nkululeko@gmail.com>
2022-10-27 14:27:50 -0700

    5-string_toupper.c

---
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * string_toupper - changes lowercase to uppercase
+ * @n: pointer
+ * Return: char
+ */
+char *string_toupper(char *n)
+{
+	int i;
+
+	i = 0;
+	while (n[i] != '\0')
+	{
+		if (n[1] >= 'a' && n[1] <= 'z')
+			n[i] = n[i] - 32;
+
+		i++;
+	}
+	return (n);
+}
