d5b6f3b385152b185869d72b22e2adff07ec02cb
kingswaysales <marco.nkululeko@gmail.com>
2022-11-02 14:21:01 -0700

    8-print_diagsums.c

--- 0x07-pointers_arrays_strings/8-print_diagsums.c
+++ 0x07-pointers_arrays_strings/8-print_diagsums.c
@@ -3,28 +3,19 @@
 
 /**
  * print_diagsums - Print the sum of two diagnolas of square matrix
- * 			of squa matrix of integers
  * @a: the matrix
  * @size: the size
  * Return: Nothing
  */
 void print_diagsums(int *a, int size)
 {
-	int index, sum1 = 0, sum2 = 0;
+	int b, sum1 = 0, sum2 = 0;
 
-	for (index = 0; index < size; index++)
+	for (b = 0; b < size; b++)
 	{
-		sum1 += a[index];
-		a += size;
+		sum1 += a[(size + 1) * b];
+		sum2 += a[(size - 1) * (b + 1)];
 	}
 
-	a -= size;
-
-	for (index = 0; index < size; index++)
-	{
-		sum2 += a[index];
-		a -= size;
-	}
-
-	printf("%d, %d\n", sum2, sum2);
+	printf("%d, %d\n", sum1, sum2);
 }
