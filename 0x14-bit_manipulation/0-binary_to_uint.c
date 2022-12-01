8e6dca4bb4454471dbcada81d7bf5915813c8cbf
kingswaysales <marco.nkululeko@gmail.com>
2022-12-01 14:04:25 -0800

    0-binary_to_uint.c

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -8,16 +8,22 @@
 unsigned int binary_to_uint(const char *b)
 {
 	int i;
-	unsigned int nas = 0;
+	unsigned int num;
 
+	num = 0;
 	if (!b)
 		return (0);
-	for (i = 0; b[i]; i++)
+	for (i = 0; b[i] != '\0'; i++)
 	{
-		if (b[i] < '0' || b[i] > '1')
+		if (b[i] != '0' && b[i] != '1')
 			return (0);
-		nas = 2 * nas + (b[i] - '0');
+	}
+	for (i = 0; b[i] != '\0'; i++)
+	{
+		num <<= 1;
+		if (b[i] == '1')
+			num += 1;
 	}
 
-	return (nas);
+	return (num);
 }
