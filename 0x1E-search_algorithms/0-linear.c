b566c2eaec58dca483dea0bfefde1a82e805f501
kingswaysales <marco.nkululeko@gmail.com>
2023-09-12 10:28:47 -0700

    task0

--- 0x1E-search_algorithms/0-linear.c
+++ 0x1E-search_algorithms/0-linear.c
@@ -1,24 +1,30 @@
 #include "search_algos.h"
 
 /**
- * linear_search - Uses a linear search algorithm to find a value in an array
- * @array: pointer to first element of array to search in
- * @size: size of array
- * @value: value to search for
+ * linear_search - Function that searches for a value in an
+ * array of integers using the Linear search algorithm
  *
- * Return: first index of value, or -1 if array is NULL or value not found
+ * @array: Array is a pointer to the first element of the array to search in
+ * @size: Number of elements in array
+ * @value: Value to search for in the array
+ *
+ * Return: The first index where value is located
+ * or -1 if value is not found or if array is NULL
  */
 int linear_search(int *array, size_t size, int value)
 {
-	size_t i;
+	size_t i = 0;
 
-	if (array == NULL)
-		return (-1);
-	for (i = 0; i < size; i++)
+	if (array != NULL)
 	{
-		printf("Value checked array[%lu] = [%d]\n", i, array[i]);
-		if (array[i] == value)
-			return (i);
+		for (i = 0; i < size; i++)
+		{
+			printf("Value checked array[%lu] = [%d]\n", i, array[i]);
+			if (array[i] == value)
+			{
+				return (i);
+			}
+		}
 	}
 	return (-1);
 }
--- 0x1E-search_algorithms/search_algos.h
+++ 0x1E-search_algorithms/search_algos.h
@@ -1,7 +1,9 @@
-#ifndef __search__
-#define __search__
+#ifndef SEARCH_H
+#define SEARCH_H
+
 #include <stdlib.h>
 #include <stdio.h>
+#include <math.h>
 
 /**
  * struct listint_s - singly linked list
@@ -20,35 +22,50 @@ typedef struct listint_s
 	struct listint_s *next;
 } listint_t;
 
-/**
- * struct skiplist_s - Singly linked list with an express lane
- *
- * @n: Integer
- * @index: Index of the node in the list
- * @next: Pointer to the next node
- * @express: Pointer to the next node in the express lane
- *
- * Description: singly linked list node structure with an express lane
- * for Holberton project
+
+/*
+ * Function that searches for a value in an
+ * array of integers using the Linear search algorithm
  */
-typedef struct skiplist_s
-{
-	int n;
-	size_t index;
-	struct skiplist_s *next;
-	struct skiplist_s *express;
-} skiplist_t;
-
-int bin_search_recursive(int *array, size_t start, size_t end, int value);
-int interpolation_recursive(int *array, size_t low, size_t high, int value);
-
 int linear_search(int *array, size_t size, int value);
-int binary_search(int *array, size_t size, int value);
-int jump_search(int *array, size_t size, int value);
-int interpolation_search(int *array, size_t size, int value);
-int exponential_search(int *array, size_t size, int value);
-int advanced_binary(int *array, size_t size, int value);
-listint_t *jump_list(listint_t *list, size_t size, int value);
-skiplist_t *linear_skip(skiplist_t *list, int value);
 
-#endif /* __search__ */
+/*
+ * Function that searches for a value in a sorted array
+ * of integers using the Binary search algorithm
+ */
+int binary_search(int *array, size_t size, int value);
+
+/*
+ * Function that searches for a value in a
+ * sorted array of integers using the Jump search algorithm
+ */
+int jump_search(int *array, size_t size, int value);
+
+/*
+ * Function that searches for a value in a sorted array of
+ * integers using the Interpolation search algorithm
+ */
+int interpolation_search(int *array, size_t size, int value);
+
+/*
+ * Function that searches for a value in a sorted array
+ * of integers using the Exponential search algorithm
+ */
+int exponential_search(int *array, size_t size, int value);
+
+/*
+ * Function that searches for a value in a sorted list
+ * of integers using the Jump search algorithm in a singly linked list
+ */
+listint_t *jump_list(listint_t *list, size_t size, int value);
+
+ /* print_list - Prints the content of a listint_t */
+void print_list(const listint_t *list);
+
+/* create_list - Creates a single linked list */
+listint_t *create_list(int *array, size_t size);
+
+/* free_list - Deallocates a singly linked list */
+void free_list(listint_t *list);
+
+#endif /* SEARCH_H */
